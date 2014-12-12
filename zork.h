c0a131af47a9a7b11ae3c2490f7421318f529f2d
hxiong <hxiong@purdue.edu>
2014-12-12 13:10:57 -0500

    parser works

---
+++ zork.h
@@ -0,0 +1,11 @@
+#ifndef ZORK_H
+#define ZORK_H
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+
+using namespace rapidxml;
+
+void MapTraverse(xml_node<> *); // traverse the child nodes of "map"
+void RoomTraverse(xml_node<> *);  // traverse the child nodes of "room"
+
+#endif
